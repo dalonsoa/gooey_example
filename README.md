942ffd226307784ed59cc0aa2af935649136fda3
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-04-27 22:29:32 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# gooey_example
+# Gooey Example
 Example of packaging a CLI app into an executable with Gooey and PyInstaller
+
+1) Create and activate a virtual environment
+2) run ```pip install -r requirements.txt```
+3) To check the application run ```python src/main.py```
+4) To package the application run:
+    - Windows: ```pyinstaller -F --windowed build-win.spec```
+    - MAcOS: ```pyinstaller -F --windowed build-osx.spec```
+
---
+++ src/main.py
@@ -0,0 +1,29 @@
+import argparse
+from gooey import Gooey
+
+
+@Gooey
+def main():
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument(
+        "integers",
+        metavar="N",
+        type=int,
+        nargs="+",
+        help="integer values for the accumulator",
+    )
+    parser.add_argument(
+        "--sum",
+        dest="accumulate",
+        action="store_const",
+        const=sum,
+        default=max,
+        help="sum the integers (default: find the max)",
+    )
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
+
+if __name__ == "__main__":
+    main()
